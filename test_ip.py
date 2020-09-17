bea18b5fb341a422b1a3c01f6692ec9ecca81502
ke_yoshida <yoshida.keisuke@mic-p.com>
2020-09-17 19:11:45 +0900

    てｓｔ

--- test_ip.py
+++ test_ip.py
@@ -2,7 +2,7 @@ import ipget
 import datetime
 
 Datetime = datetime.datetime.today()    # 日付
-IP = ipget.ipget()   # 親機IP
+IP = ipget.ipget()   # 親機IPよ
 BaseUnit = IP.ipaddr("wlan0")
 
 print(Datetime)
